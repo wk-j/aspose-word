ad2e21befc8407d48000237e4ce9b7ac2ad7ff75
wk-j <somnuk.wk@gmail.com>
2019-05-13 19:37:14 +0700

    A

--- net/Hello/Program.cs
+++ net/Hello/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using Aspose.Words;
 
-namespace Hello
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Hello {
+    class Program {
+        static void Main(string[] args) {
+            var dataDir = "resource/";
+            Document doc = new Document(dataDir + "A.docx");
+
+            //FontSettings fontSettings = new FontSettings();
+            //fontSettings.FallbackSettings.Load(dataDir + "Fallback.xml");
+
+            // Set font settings
+            //doc.FontSettings = fontSettings;
+            dataDir = dataDir + "A.pdf";
+            doc.Save(dataDir);
         }
     }
 }
